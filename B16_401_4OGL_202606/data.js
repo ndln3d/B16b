7ec3947f8694a85cd1f8440d5366bcc42edb1854
ndln3d <3d@ndln.ch>
2026-06-11 08:56:02 +0200

    commit
    commit

---
+++ B16_401_4OGL_202606/data.js
@@ -0,0 +1,461 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-esszimmer",
+      "name": "Esszimmer",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8488203364285702,
+          "pitch": 0.0565199179451632,
+          "rotation": 0,
+          "target": "5-bro--schlafzimmer-2"
+        },
+        {
+          "yaw": 2.7939955896818844,
+          "pitch": 0.1020018172844459,
+          "rotation": 0,
+          "target": "7-kueche"
+        },
+        {
+          "yaw": -2.6894251819849586,
+          "pitch": 0.1399145759945899,
+          "rotation": 0,
+          "target": "4-wc"
+        },
+        {
+          "yaw": -2.4837123627536872,
+          "pitch": 0.13721715967631987,
+          "rotation": 0,
+          "target": "3-badezimmer"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-salon",
+      "name": "Salon",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.3524594337470024,
+          "pitch": 0.13574098063600104,
+          "rotation": 0,
+          "target": "1-salon"
+        },
+        {
+          "yaw": -0.19485547875921583,
+          "pitch": 0.21068061318103126,
+          "rotation": 0,
+          "target": "8-balkon"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-schlafzimmer-1",
+      "name": "Schlafzimmer 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.882092618085185,
+          "pitch": 0.17232821906271134,
+          "rotation": 0,
+          "target": "1-salon"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-badezimmer",
+      "name": "Badezimmer",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6286001883944934,
+          "pitch": 0.25514675601056425,
+          "rotation": 0,
+          "target": "1-salon"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-wc",
+      "name": "WC",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.9960786986540064,
+          "pitch": 0.2564639963749986,
+          "rotation": 0,
+          "target": "1-salon"
+        },
+        {
+          "yaw": -1.3389479977297682,
+          "pitch": 0.2601363824627292,
+          "rotation": 0,
+          "target": "0-esszimmer"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-bro--schlafzimmer-2",
+      "name": "Büro / Schlafzimmer 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.5232223391932305,
+          "pitch": 0.1946533189900883,
+          "rotation": 0,
+          "target": "0-esszimmer"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-dachstock",
+      "name": "Dachstock",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.5323053686478723,
+          "pitch": 0.1290159451374322,
+          "rotation": 0,
+          "target": "9-dachterasse"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-kueche",
+      "name": "Kueche",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.16635131706544826,
+          "pitch": 0.20618759346876026,
+          "rotation": 0,
+          "target": "0-esszimmer"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-balkon",
+      "name": "Balkon",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.910743181277624,
+          "pitch": 0.12062032372442033,
+          "rotation": 0,
+          "target": "1-salon"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-dachterasse",
+      "name": "Dachterasse",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.06184182735012911,
+          "pitch": 0.14715420343674168,
+          "rotation": 0,
+          "target": "6-dachstock"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "B16 401",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
